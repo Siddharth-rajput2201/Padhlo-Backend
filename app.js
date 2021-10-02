9450d9ee044d30664f0d1b9e73a43bf10f2f95fa
Hush <siddharth@rapidalley.com>
2021-10-02 17:21:00 +0530

    first commit

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.listen(port, function () {
     console.log('Server rocking on port 3000');
 })
 
-mongoose.connect(process.env.DB_HOST_URL,{ useNewUrlParser: true ,useUnifiedTopology: true})
-//mongoose.connect("mongodb://localhost:27017/padhloDB",{ useNewUrlParser: true ,useUnifiedTopology: true})
+//mongoose.connect(process.env.DB_HOST_URL,{ useNewUrlParser: true ,useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/padhloDB",{ useNewUrlParser: true ,useUnifiedTopology: true})
 
 console.log("Connected to database");
 
